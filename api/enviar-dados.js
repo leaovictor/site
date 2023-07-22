fb478f489d4968c40748a86c0780601e8647d59d
Victor Leão <110788743+victorleaoQA@users.noreply.github.com>
2023-07-22 12:00:49 -0300

    wip

--- api/enviar-dados.js
+++ api/enviar-dados.js
@@ -1,11 +1,8 @@
-// NOVO CÓDIGO PARA TAMBÉM ENVIAR PRO MEU E-MAIL
-
 const { MongoClient } = require("mongodb");
-const enviarEmail = require("./enviar-email"); // Importe a função enviarEmail
+const enviarEmail = require("./enviar-email");
 const moment = require("moment-timezone");
 
 async function conectarAoMongoDB(req) {
-  // Adicione o parâmetro 'req' aqui
   const uri = process.env.MONGODB_URI;
   const dbName = process.env.MONGODB_DB;
 
@@ -16,23 +13,14 @@ async function conectarAoMongoDB(req) {
     console.log("Conectado ao MongoDB");
 
     const db = client.db(dbName);
+    const collection = db.collection("contato");
 
-    const collection = db.collection("contato"); // Substitua 'nome-da-colecao' pelo nome da coleção que deseja usar
-
-    const dadosFormulario = req.body; // Supondo que os dados do formulário estejam disponíveis no objeto req.body
-
-    // Obter a data e hora atual do envio do formulário no fuso horário de Brasília
+    const dadosFormulario = req.body;
     const dataEnvio = moment().tz("America/Sao_Paulo").format();
-
     dadosFormulario.shipping = dataEnvio;
 
-    // Inserir os dados na coleção
     const resultadoInsercao = await collection.insertOne(dadosFormulario);
-
-    console.log(
-      "Dados do formulário inseridos com sucesso:",
-      resultadoInsercao
-    );
+    console.log("Dados do formulário inseridos com sucesso:", resultadoInsercao);
   } catch (error) {
     console.error("Erro ao conectar ao MongoDB:", error);
   } finally {
@@ -45,16 +33,10 @@ module.exports = async (req, res) => {
   try {
     await conectarAoMongoDB(req);
 
-    // Chama a função enviarEmail passando os dados do formulário no objeto req.body
     const resultadoEnvioEmail = await enviarEmail(req.body);
 
-    // Verifica se o email foi enviado com sucesso e envia a resposta para o cliente
     if (resultadoEnvioEmail.success) {
-      res
-        .status(200)
-        .send(
-          "Dados do formulário recebidos, inseridos no MongoDB e email enviado com sucesso!"
-        ).redirect("../thankyou.html");
+      res.status(200).send("Dados do formulário recebidos, inseridos no MongoDB e email enviado com sucesso!");
     } else {
       res.status(500).send("Ocorreu um erro ao processar a solicitação.");
     }
@@ -62,4 +44,7 @@ module.exports = async (req, res) => {
     console.error("Erro ao processar a solicitação:", error);
     res.status(500).send("Ocorreu um erro ao processar a solicitação");
   }
+
+  // Redirecionar para a página de agradecimento após o envio do formulário
+  res.redirect('../thankyou.html');
 };
