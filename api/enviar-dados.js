63979a87e41e94cf536e59d599be9a250d9f3a37
Victor Leão <110788743+victorleaoQA@users.noreply.github.com>
2023-07-22 12:33:22 -0300

    wip

--- api/enviar-dados.js
+++ api/enviar-dados.js
@@ -1,51 +1,10 @@
-// const { MongoClient } = require("mongodb");
-// const enviarEmail = require("./enviar-email");
-// const moment = require("moment-timezone");
-
-// async function conectarAoMongoDB(req) {
-//   const uri = process.env.MONGODB_URI;
-//   const dbName = process.env.MONGODB_DB;
-
-//   const client = new MongoClient(uri, { useUnifiedTopology: true });
-
-//   try {
-//     await client.connect();
-//     console.log("Conectado ao MongoDB");
-
-//     const db = client.db(dbName);
-//     const collection = db.collection("contato");
-
-//     const dadosFormulario = req.body;
-//     const dataEnvio = moment().tz("America/Sao_Paulo").format();
-//     dadosFormulario.shipping = dataEnvio;
-
-//     const resultadoInsercao = await collection.insertOne(dadosFormulario);
-//     console.log("Dados do formulário inseridos com sucesso:", resultadoInsercao);
-
-//     // Chama a função enviarEmail passando os dados do formulário no objeto req.body
-//     const resultadoEnvioEmail = await enviarEmail(req.body);
-
-//     // Verifica se o email foi enviado com sucesso e envia a resposta para o cliente
-//     if (resultadoEnvioEmail.success) {
-//       res.status(200).send("Dados do formulário recebidos, inseridos no MongoDB e email enviado com sucesso!");
-//     } else {
-//       res.status(500).send("Ocorreu um erro ao enviar o e-mail.").redirect('../thankyou.html');
-//     }
-//   } catch (error) {
-//     console.error("Erro ao processar a solicitação:", error);
-//     res.status(500).send("Ocorreu um erro ao processar a solicitação");
-//   } finally {
-//     await client.close();
-//     console.log("Conexão com o MongoDB encerrada");
-//   }
-// }
-
 
 // NOVO CÓDIGO PARA TAMBÉM ENVIAR PRO MEU E-MAIL
 
 const { MongoClient } = require("mongodb");
 const enviarEmail = require("./enviar-email"); // Importe a função enviarEmail
 const moment = require('moment-timezone');
+const path = require('path');
 
 async function conectarAoMongoDB(req) {
   // Adicione o parâmetro 'req' aqui
@@ -103,6 +62,26 @@ module.exports = async (req, res) => {
     res.status(500).send("Ocorreu um erro ao processar a solicitação");
   }
 
+};
+
+module.exports = async (req, res) => {
+  try {
+    await conectarAoMongoDB(req);
+
+    // Chama a função enviarEmail passando os dados do formulário no objeto req.body
+    const resultadoEnvioEmail = await enviarEmail(req.body);
+
+    // Verifica se o email foi enviado com sucesso e envia a resposta para o cliente
+    if (resultadoEnvioEmail.success) {
+      // Redirecionar para a página de agradecimento após o envio do formulário
+      res.redirect('/api/thankyou.html');
+    } else {
+      res.status(500).send("Ocorreu um erro ao processar a solicitação.");
+    }
+  } catch (error) {
+    console.error("Erro ao processar a solicitação:", error);
+    res.status(500).send("Ocorreu um erro ao processar a solicitação");
+  }
 };
 
 
